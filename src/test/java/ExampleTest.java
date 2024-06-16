b4b78d0780bbd885c3f30a9f442f033e72dd3723
Emir Hamulic <hamulicemir@icloud.com>
2024-06-16 13:45:15 +0200

    added checkstyle and pom file to the project

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ExampleTest {
+    @Test
+    public void testAddition() {
+        int result = 3 + 2;
+        assertEquals(5, result);
+    }
+}
