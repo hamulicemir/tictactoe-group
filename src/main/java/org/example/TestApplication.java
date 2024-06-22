b69eb23b302d14e6685c1550eee2eadca8bcf83c
Emir Hamulic <hamulicemir@icloud.com>
2024-06-22 20:39:58 +0200

    Verbesserung der Main

--- src/main/java/org/example/TestApplication.java
+++ src/main/java/org/example/TestApplication.java
@@ -23,9 +23,16 @@ public class TestApplication {
                 break;
             }
 
-            System.out.println("Player " + game.getCurrentPlayer().getMarker() + ", enter your move (row and column): ");
-            int row = scanner.nextInt();
-            int col = scanner.nextInt();
+            int row, col;
+            do {
+                System.out.println("Player " + game.getCurrentPlayer().getMarker() + ", enter your move (row and column) (1-3): ");
+                row = scanner.nextInt() - 1;
+                col = scanner.nextInt() - 1;
+
+                if (row < 0 || row > 2 || col < 0 || col > 2) {
+                    System.out.println("Invalid input. Please enter values between 1 and 3.");
+                }
+            } while (row < 0 || row > 2 || col < 0 || col > 2);
 
             if (!game.getBoard().place(row, col, game.getCurrentPlayer().getMarker())) {
                 System.out.println("This move is not valid");
@@ -33,7 +40,6 @@ public class TestApplication {
                 game.switchCurrentPlayer();
             }
         }
-
         System.out.println("Final board:");
         System.out.println(game.getBoard().print());
         scanner.close();
